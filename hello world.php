87c3b598529e086458c2849700e85453595cad4e
klevish <klevishdean@gmail.com>
2020-01-28 18:10:51 +0100

    MAJ

---
+++ hello world.php
@@ -0,0 +1,21 @@
+<?php 
+echo "hii";
+echo "hello worl";
+
+
+
+?>
+
+1/creer un repo sur git
+2/ creer un repo sur ton ordi
+3/ initialiser git => git init sur terminal
+3vis/ ajouter l'adress du server => git remote add origine'<url>
+4/ creer un fichier
+5/ verifier l'état des fichiers => git status
+6/ git add(listes des fichiers) => git add 
+7/ creer un snapshot => git commit -m "message"
+8/ recuperer les snapshot des autres dev => git pull
+9/ resolution des eventuelles problemes
+10/ envoyer les snapshots sur le server => git push
+
+pour cacher un fichier cacher .devant le fichier et pour le voir ls -a
